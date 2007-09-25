81468533023b14d8a09cc55a8ffa658557e7839a
kkowalczyk <kkowalczyk@4b1666b9-9919-0410-b7be-41930ee49dfe>
2007-09-25 04:08:52 +0000

    understand human suffixes for size like k, kb, m, mb, g and gb (kilobyte, megabyte and gigabyte respectively)
    git-svn-id: http://kjk.googlecode.com/svn/trunk@54 4b1666b9-9919-0410-b7be-41930ee49dfe

--- scripts/create_file.py
+++ scripts/create_file.py
@@ -1,5 +1,24 @@
 import sys
 
+def ends_with_no_case_and_strip(s, suff_arr):
+	s = s.lower()
+	for suff in suff_arr:
+		suff = suff.lower()
+		if s.endswith(suff):
+			s = s[:-len(suff)]
+			return (s, True)
+	return (s, False)
+		
+def file_size_human_decode(s):
+	(s, is_kb) = ends_with_no_case_and_strip(s, ["k", "kb"])
+	(s, is_mb) = ends_with_no_case_and_strip(s, ["m", "mb"])
+	(s, is_gb) = ends_with_no_case_and_strip(s, ["g", "gb"])
+	val = float(s)
+	if is_kb: val = val * 1024
+	if is_mb: val = val * 1024 * 1024
+	if is_gb: val = val * 1024 * 1024 * 1024
+	return int(val)
+
 def usage_and_exit():
 	print "Usage: create_file.py file_name file_size"
 
@@ -7,7 +26,7 @@ def main():
 	if 3 != len(sys.argv):
 		usage_and_exit()
 	file_name = sys.argv[1]
-	file_size = int(sys.argv[2])
+	file_size = file_size_human_decode(sys.argv[2])
 	fo = open(file_name, "wb")
 	data = "a" * 4096
 	data_size = len(data)
