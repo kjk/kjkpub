9143c9ee698273b1895a2df5599efe221865b452
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-01-03 20:44:38 -0800

    more detailed information

--- DotNetDllSummary/Program.cs
+++ DotNetDllSummary/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,17 +10,69 @@ namespace DotNetDllSummary
 {
     class GenerateSummary
     {
+        public string[] namespacesToIgnore = new string[] {
+            "Microsoft.Build",
+            "Microsoft.Aspnet",
+            "Microsoft.JScript",
+            "Microsoft.VisualBasic",
+            "Microsoft.VisualC",
+            "Microsoft.Vsa"
+        };
+
+        public bool IsIgnoredNamespace(string s)
+        {
+            foreach (var ns in namespacesToIgnore)
+            {
+                if (s.StartsWith(ns))
+                    return true;
+            }
+            return false;
+        }
+
+        public string ShortenTypes(string s)
+        {
+            var rd = new string[] {
+                "System.Void", "void",
+                "System.Object", "object",
+                "System.Char", "char",
+                "System.String", "string",
+                "System.Byte", "byte",
+                "System.Guid", "guid",
+                "System.Int16", "i16",
+                "System.Int32", "i32",
+                "System.Int64", "i64",
+                "System.UInt16", "u16",
+                "System.UInt32", "u32",
+                "System.UInt64", "u64",
+                "System.Boolean", "bool",
+                "System.Double", "double",
+                "System.Single", "float",
+                "System.Decimal", "decimal",
+                "System.DateTime", "datetime",
+                "System.Array", "array",
+                "System.Type", "type"
+            };
+            for (int i = 0; i < rd.Length / 2; i++)
+            {
+                s = s.Replace(rd[i*2], rd[i*2 + 1]);
+            }
+            s = s.Replace(",", ", ");
+            return s;
+        }
+
         public void DumpField(FieldDefinition field)
         {
             if (!field.IsPublic)
                 return;
-            if (field.IsSpecialName || field.IsRuntimeSpecialName)
+            if (field.IsRuntimeSpecialName)
+                Debug.Assert(field.IsSpecialName);
+            if (field.IsSpecialName)
                 return;
-            var s = " F: " + field.FullName;
+            var s = " F: " + ShortenTypes(field.FieldType.FullName) + " " + field.Name;
             System.Console.WriteLine(s);
         }
 
-        public void DumpFields(TypeDefinition type)
+        public void DumpTypeFields(TypeDefinition type)
         {
             if (!type.HasFields)
                 return;
@@ -29,15 +82,81 @@ namespace DotNetDllSummary
             }
         }
 
+        public void DumpEvent(EventDefinition e)
+        {
+            if (e.IsSpecialName)
+                return;
+            var s = " E: " + ShortenTypes(e.EventType.FullName) + " " + e.Name;
+            System.Console.WriteLine(s);
+        }
+
+        public void DumpTypeEvents(TypeDefinition type)
+        {
+            if (!type.HasEvents)
+                return;
+            foreach (EventDefinition e in type.Events)
+            {
+                DumpEvent(e);
+            }
+        }
+
         public void DumpEnum(TypeDefinition type)
         {
+            if (IsIgnoredNamespace(type.FullName))
+                return;
             var s = "E:" + type.FullName;
             if (type.BaseType != null)
             {
-                s = s + " : " + type.BaseType.FullName;
+                s = s + " : " + ShortenTypes(type.BaseType.FullName);
             }
             Console.WriteLine(s);
-            DumpFields(type);
+            DumpTypeFields(type);
+        }
+
+        public string GetMethodName(MethodDefinition m)
+        {
+            string s = "";
+            if (m.IsStatic)
+                s += "static ";
+            if (m.IsFinal)
+                s += "final ";
+            if (m.IsAbstract)
+                s += "abstract ";
+            if (m.IsVirtual)
+                s += "virtual ";
+            s += ShortenTypes(m.ReturnType.FullName) + " ";
+            s += m.Name + "(";
+            if (m.HasParameters)
+            {
+                bool first = true;
+                foreach (var p in m.Parameters)
+                {
+                    if (first)
+                        first = false;
+                    else
+                        s += ", ";
+                    s += ShortenTypes(p.ParameterType.FullName) + " " + p.Name;
+                }
+            }
+            return s + ")";
+        }
+
+        public void DumpMethod(MethodDefinition m)
+        {
+            if (m.IsPrivate)
+                return;
+            if (m.IsRuntimeSpecialName)
+                Debug.Assert(m.IsSpecialName);
+            if (m.IsSpecialName)
+                return;
+#if NOT_DISABLED
+            if (m.Name.StartsWith("add_") || m.Name.StartsWith("remove_"))
+            {
+                Debug.Assert(false);
+                continue;
+            }
+#endif
+            Console.WriteLine(" M:" + GetMethodName(m));
         }
 
         public void DumpTypeMethods(TypeDefinition type)
@@ -46,27 +165,74 @@ namespace DotNetDllSummary
                 return;
             foreach (var m in type.Methods)
             {
-                if (!m.IsPublic)
-                    continue;
-                Console.WriteLine(" M:" + m.FullName);
+                DumpMethod(m);
             }
+        }
+
+        public string GetPropertyName(PropertyDefinition p)
+        {
+            // TODO: more detailed?
+            string s = ShortenTypes(p.PropertyType.FullName);
+            s += " " + p.Name;
+            return s;
+        }
+
+        public void DumpTypeProperties(TypeDefinition type)
+        {
+            if (!type.HasProperties)
+                return;
+            foreach (PropertyDefinition p in type.Properties)
+            {
+                if (p.IsRuntimeSpecialName)
+                    Debug.Assert(p.IsSpecialName);
+                if (p.IsSpecialName)
+                    continue;
+                Console.WriteLine(" P:" + GetPropertyName(p));
+            }
+        }
+
+        public string GetTypeName(TypeDefinition type)
+        {
+            var s = type.FullName;
+            TypeDefinition t = type;
+            while (t.BaseType != null)
+            {
+                s = s + " : " + ShortenTypes(t.BaseType.FullName);
+                TypeReference tr = t.BaseType;
+                t = tr.Resolve();
+            }
+            return s;
         }
 
         public void DumpClass(TypeDefinition type)
         {
-            Console.WriteLine("C:" + type.FullName);
+            if (IsIgnoredNamespace(type.FullName))
+                return;
+            Console.WriteLine("C:" + GetTypeName(type));
+            DumpTypeFields(type);
+            DumpTypeProperties(type);
+            DumpTypeEvents(type);
             DumpTypeMethods(type);
         }
 
         public void DumpValueType(TypeDefinition type)
         {
-            Console.WriteLine("VT:" + type.FullName);
+            if (IsIgnoredNamespace(type.FullName))
+                return;
+            Console.WriteLine("S:" + GetTypeName(type));
+            DumpTypeFields(type);
+            DumpTypeProperties(type);
             DumpTypeMethods(type);
         }
 
         public void DumpInterface(TypeDefinition type)
         {
+            if (IsIgnoredNamespace(type.FullName))
+                return;
+            Debug.Assert(type.BaseType == null);
+            Debug.Assert(!type.HasFields);
             Console.WriteLine("I:" + type.FullName);
+            DumpTypeProperties(type);
             DumpTypeMethods(type);
         }
 
@@ -132,9 +298,9 @@ namespace DotNetDllSummary
     {
         static void Main(string[] args)
         {
-            string dir = @"c:\Windows\Microsoft.NET\Framework\v4.0.30319";
             var gs = new GenerateSummary();
-            gs.SummaryForDir(dir);
+            gs.SummaryForDir(@"c:\Windows\Microsoft.NET\Framework\v4.0.30319");
+            gs.SummaryForDir(@"c:\Windows\Microsoft.NET\Framework\v4.0.30319\WPF");
         }
     }
 }
