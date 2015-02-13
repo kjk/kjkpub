849b78e6d97e0fe776b7105e1d8e447d31656cbe
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-02-12 23:17:40 -0800

    add sample showing how to use personal access token

---
+++ go/github_sample/sample_2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+var (
+	// you need to generate personal access token at
+	// https://github.com/settings/applications#personal-access-tokens
+	personalAccessToken = ""
+)
+
+type TokenSource struct {
+	AccessToken string
+}
+
+func (t *TokenSource) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: t.AccessToken,
+	}
+	return token, nil
+}
+
+func main() {
+	tokenSource := &TokenSource{
+		AccessToken: personalAccessToken,
+	}
+	oauthClient := oauth2.NewClient(nil, tokenSource)
+	client := github.NewClient(oauthClient)
+	user, _, err := client.Users.Get("")
+	if err != nil {
+		fmt.Printf("client.Users.Get() faled with '%s'\n", err)
+		return
+	}
+	d, err := json.MarshalIndent(user, "", "  ")
+	if err != nil {
+		fmt.Printf("json.MarshlIndent() failed with %s\n", err)
+		return
+	}
+	fmt.Printf("User:\n%s\n", string(d))
+}
