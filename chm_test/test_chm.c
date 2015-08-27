b74efad2bf0b2b38895616153f39d86434cabf81
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-08-26 20:03:39 -0700

    clang reformat

--- chm_test/test_chm.c
+++ chm_test/test_chm.c
@@ -16,76 +16,72 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int _print_ui(struct chmFile *h,
-              struct chmUnitInfo *ui,
-              void *context)
-{
+int _print_ui(struct chmFile *h, struct chmUnitInfo *ui, void *context) {
     static char szBuf[128];
     memset(szBuf, 0, 128);
-    if(ui->flags & CHM_ENUMERATE_NORMAL)
+    if (ui->flags & CHM_ENUMERATE_NORMAL)
         strcpy(szBuf, "normal ");
-    else if(ui->flags & CHM_ENUMERATE_SPECIAL)
+    else if (ui->flags & CHM_ENUMERATE_SPECIAL)
         strcpy(szBuf, "special ");
-    else if(ui->flags & CHM_ENUMERATE_META)
+    else if (ui->flags & CHM_ENUMERATE_META)
         strcpy(szBuf, "meta ");
 
-    if(ui->flags & CHM_ENUMERATE_DIRS)
+    if (ui->flags & CHM_ENUMERATE_DIRS)
         strcat(szBuf, "dir");
-    else if(ui->flags & CHM_ENUMERATE_FILES)
+    else if (ui->flags & CHM_ENUMERATE_FILES)
         strcat(szBuf, "file");
 
-    printf("   %1d %8d %8d   %s\t\t%s\n",
-           (int)ui->space,
-           (int)ui->start,
-           (int)ui->length,
-           szBuf,
+    printf("   %1d %8d %8d   %s\t\t%s\n", (int)ui->space, (int)ui->start, (int)ui->length, szBuf,
            ui->path);
     return CHM_ENUMERATOR_CONTINUE;
 }
 
-// TODO: doesn't seem to be detected
 // https://code.google.com/p/address-sanitizer/wiki/ExampleUseAfterFree
 int trigger_use_after_free(int i) {
-  int *arr = (int*)malloc(sizeof(int)*100);
-  free(arr);
-  return arr[i]; // BOOM
+    int *arr = (int *)malloc(sizeof(int) * 100);
+    free(arr);
+    return arr[i]; // BOOM
 }
 
-// TODO: doesn't seem to be detected
+// TODO: doesn't seem to be detected by /Applications/Xcode-beta.app//Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang from XCode 7.0 b3
 // https://code.google.com/p/address-sanitizer/wiki/ExampleStackOutOfBounds
 int trigger_stack_out_of_bounds(int i) {
-  int stack_array[100];
-  stack_array[1] = 0;
-  return stack_array[i + 100];  // BOOM
+    int stack_array[100];
+    stack_array[1] = 0;
+    return stack_array[i + 100]; // BOOM
 }
 
-int main(int argc, char **argv)
-{
+// https://code.google.com/p/address-sanitizer/wiki/ExampleHeapOutOfBounds
+int trigger_heap_out_of_bounds(int i) {
+    int *arr = (int *)malloc(sizeof(int) * 100);
+    arr[0] = 0;
+    int res = arr[i + 100];  // BOOM
+    free(arr);
+    return res;
+}
+
+int main(int argc, char **argv) {
     struct chmFile *h;
     char *filePath;
 
     //trigger_use_after_free(5);
-    //trigger_stack_out_of_bounds(1);
+    //trigger_stack_out_of_bounds(5);
+    //trigger_heap_out_of_bounds(1);
 
-    if (argc != 2)
-    {
+    if (argc != 2) {
         fprintf(stderr, "usage: %s <chmfile>\n", argv[0]);
         exit(1);
     }
 
     filePath = argv[1];
     h = chm_open(filePath);
-    if (h == NULL)
-    {
+    if (h == NULL) {
         fprintf(stderr, "failed to open %s\n", filePath);
         exit(1);
     }
 
     printf("%s:\n", filePath);
-    int res = chm_enumerate(h,
-                        CHM_ENUMERATE_ALL,
-                        _print_ui,
-                        NULL);
+    int res = chm_enumerate(h, CHM_ENUMERATE_ALL, _print_ui, NULL);
 
     if (CHM_ENUMERATOR_FAILURE == res) {
         printf("   *** ERROR *** %d: \n", res);
