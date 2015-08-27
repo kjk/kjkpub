c6f30cc6d0c1776010e73dcd1d71644effe70a21
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-08-26 20:27:21 -0700

    update scripts/clang-fmt.sh to format all code

--- chm_test/scripts/clang-fmt.sh
+++ chm_test/scripts/clang-fmt.sh
@@ -5,3 +5,5 @@ set -o errexit
 set -o pipefail
 
 clang-format -style="{BasedOnStyle: Mozilla, IndentWidth: 4, ColumnLimit: 100}" -i *.c
+clang-format -style="{BasedOnStyle: Mozilla, IndentWidth: 4, ColumnLimit: 100}" -i CHMLib/src/*
+clang-format -style="{BasedOnStyle: Mozilla, IndentWidth: 4, ColumnLimit: 100}" -i CHMLib/examples/*.c
