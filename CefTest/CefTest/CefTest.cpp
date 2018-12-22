26814236aff7d128f258d635fa362a5b82ab87ba
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-12-22 13:30:39 -0800

    more stuff in CefTest.cpp

--- CefTest/CefTest/CefTest.cpp
+++ CefTest/CefTest/CefTest.cpp
@@ -1,6 +1,322 @@
 #include "stdafx.h"
 #include "CefTest.h"
 
+#include <string>
+#include <list>
+#include <sstream>
+
+#include "include/base/cef_bind.h"
+
+#include "include/cef_app.h"
+#include "include/cef_browser.h"
+#include "include/cef_client.h"
+#include "include/cef_command_line.h"
+#include "include/views/cef_window.h"
+#include "include/views/cef_browser_view.h"
+#include "include/views/cef_window.h"
+#include "include/wrapper/cef_closure_task.h"
+#include "include/wrapper/cef_helpers.h"
+
+// Implement application-level callbacks for the browser process.
+class SimpleApp : public CefApp, public CefBrowserProcessHandler {
+public:
+	SimpleApp();
+
+	// CefApp methods:
+	virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler()
+		OVERRIDE {
+		return this;
+	}
+
+	// CefBrowserProcessHandler methods:
+	virtual void OnContextInitialized() OVERRIDE;
+
+private:
+	// Include the default reference counting implementation.
+	IMPLEMENT_REFCOUNTING(SimpleApp);
+};
+
+namespace {
+
+	// When using the Views framework this object provides the delegate
+	// implementation for the CefWindow that hosts the Views-based browser.
+	class SimpleWindowDelegate : public CefWindowDelegate {
+	public:
+		explicit SimpleWindowDelegate(CefRefPtr<CefBrowserView> browser_view)
+			: browser_view_(browser_view) {}
+
+		void OnWindowCreated(CefRefPtr<CefWindow> window) OVERRIDE {
+			// Add the browser view and show the window.
+			window->AddChildView(browser_view_);
+			window->Show();
+
+			// Give keyboard focus to the browser view.
+			browser_view_->RequestFocus();
+		}
+
+		void OnWindowDestroyed(CefRefPtr<CefWindow> window) OVERRIDE {
+			browser_view_ = NULL;
+		}
+
+		bool CanClose(CefRefPtr<CefWindow> window) OVERRIDE {
+			// Allow the window to close if the browser says it's OK.
+			CefRefPtr<CefBrowser> browser = browser_view_->GetBrowser();
+			if (browser)
+				return browser->GetHost()->TryCloseBrowser();
+			return true;
+		}
+
+	private:
+		CefRefPtr<CefBrowserView> browser_view_;
+
+		IMPLEMENT_REFCOUNTING(SimpleWindowDelegate);
+		DISALLOW_COPY_AND_ASSIGN(SimpleWindowDelegate);
+	};
+
+}  // namespace
+
+class SimpleHandler : public CefClient,
+	public CefDisplayHandler,
+	public CefLifeSpanHandler,
+	public CefLoadHandler {
+public:
+	explicit SimpleHandler(bool use_views);
+	~SimpleHandler();
+
+	// Provide access to the single global instance of this object.
+	static SimpleHandler* GetInstance();
+
+	// CefClient methods:
+	virtual CefRefPtr<CefDisplayHandler> GetDisplayHandler() OVERRIDE {
+		return this;
+	}
+	virtual CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() OVERRIDE {
+		return this;
+	}
+	virtual CefRefPtr<CefLoadHandler> GetLoadHandler() OVERRIDE { return this; }
+
+	// CefDisplayHandler methods:
+	virtual void OnTitleChange(CefRefPtr<CefBrowser> browser,
+		const CefString& title) OVERRIDE;
+
+	// CefLifeSpanHandler methods:
+	virtual void OnAfterCreated(CefRefPtr<CefBrowser> browser) OVERRIDE;
+	virtual bool DoClose(CefRefPtr<CefBrowser> browser) OVERRIDE;
+	virtual void OnBeforeClose(CefRefPtr<CefBrowser> browser) OVERRIDE;
+
+	// CefLoadHandler methods:
+	virtual void OnLoadError(CefRefPtr<CefBrowser> browser,
+		CefRefPtr<CefFrame> frame,
+		ErrorCode errorCode,
+		const CefString& errorText,
+		const CefString& failedUrl) OVERRIDE;
+
+	// Request that all existing browser windows close.
+	void CloseAllBrowsers(bool force_close);
+
+	bool IsClosing() const { return is_closing_; }
+
+private:
+	// Platform-specific implementation.
+	void PlatformTitleChange(CefRefPtr<CefBrowser> browser,
+		const CefString& title);
+
+	// True if the application is using the Views framework.
+	const bool use_views_;
+
+	// List of existing browser windows. Only accessed on the CEF UI thread.
+	typedef std::list<CefRefPtr<CefBrowser>> BrowserList;
+	BrowserList browser_list_;
+
+	bool is_closing_;
+
+	// Include the default reference counting implementation.
+	IMPLEMENT_REFCOUNTING(SimpleHandler);
+};
+
+namespace {
+
+	SimpleHandler* g_instance = NULL;
+
+}  // namespace
+
+SimpleHandler::SimpleHandler(bool use_views)
+	: use_views_(use_views), is_closing_(false) {
+	DCHECK(!g_instance);
+	g_instance = this;
+}
+
+SimpleHandler::~SimpleHandler() {
+	g_instance = NULL;
+}
+
+// static
+SimpleHandler* SimpleHandler::GetInstance() {
+	return g_instance;
+}
+
+void SimpleHandler::OnTitleChange(CefRefPtr<CefBrowser> browser,
+	const CefString& title) {
+	CEF_REQUIRE_UI_THREAD();
+
+	if (use_views_) {
+		// Set the title of the window using the Views framework.
+		CefRefPtr<CefBrowserView> browser_view =
+			CefBrowserView::GetForBrowser(browser);
+		if (browser_view) {
+			CefRefPtr<CefWindow> window = browser_view->GetWindow();
+			if (window)
+				window->SetTitle(title);
+		}
+	}
+	else {
+		// Set the title of the window using platform APIs.
+		PlatformTitleChange(browser, title);
+	}
+}
+
+void SimpleHandler::OnAfterCreated(CefRefPtr<CefBrowser> browser) {
+	CEF_REQUIRE_UI_THREAD();
+
+	// Add to the list of existing browsers.
+	browser_list_.push_back(browser);
+}
+
+bool SimpleHandler::DoClose(CefRefPtr<CefBrowser> browser) {
+	CEF_REQUIRE_UI_THREAD();
+
+	// Closing the main window requires special handling. See the DoClose()
+	// documentation in the CEF header for a detailed destription of this
+	// process.
+	if (browser_list_.size() == 1) {
+		// Set a flag to indicate that the window close should be allowed.
+		is_closing_ = true;
+	}
+
+	// Allow the close. For windowed browsers this will result in the OS close
+	// event being sent.
+	return false;
+}
+
+void SimpleHandler::OnBeforeClose(CefRefPtr<CefBrowser> browser) {
+	CEF_REQUIRE_UI_THREAD();
+
+	// Remove from the list of existing browsers.
+	BrowserList::iterator bit = browser_list_.begin();
+	for (; bit != browser_list_.end(); ++bit) {
+		if ((*bit)->IsSame(browser)) {
+			browser_list_.erase(bit);
+			break;
+		}
+	}
+
+	if (browser_list_.empty()) {
+		// All browser windows have closed. Quit the application message loop.
+		CefQuitMessageLoop();
+	}
+}
+
+void SimpleHandler::OnLoadError(CefRefPtr<CefBrowser> browser,
+	CefRefPtr<CefFrame> frame,
+	ErrorCode errorCode,
+	const CefString& errorText,
+	const CefString& failedUrl) {
+	CEF_REQUIRE_UI_THREAD();
+
+	// Don't display an error for downloaded files.
+	if (errorCode == ERR_ABORTED)
+		return;
+
+	// Display a load error message.
+	std::stringstream ss;
+	ss << "<html><body bgcolor=\"white\">"
+		"<h2>Failed to load URL "
+		<< std::string(failedUrl) << " with error " << std::string(errorText)
+		<< " (" << errorCode << ").</h2></body></html>";
+	frame->LoadString(ss.str(), failedUrl);
+}
+
+void SimpleHandler::PlatformTitleChange(CefRefPtr<CefBrowser> browser,
+	const CefString& title) {
+	CefWindowHandle hwnd = browser->GetHost()->GetWindowHandle();
+	SetWindowText(hwnd, std::wstring(title).c_str());
+}
+
+void SimpleHandler::CloseAllBrowsers(bool force_close) {
+	if (!CefCurrentlyOn(TID_UI)) {
+		// Execute on the UI thread.
+		CefPostTask(TID_UI, base::Bind(&SimpleHandler::CloseAllBrowsers, this,
+			force_close));
+		return;
+	}
+
+	if (browser_list_.empty())
+		return;
+
+	BrowserList::const_iterator it = browser_list_.begin();
+	for (; it != browser_list_.end(); ++it)
+		(*it)->GetHost()->CloseBrowser(force_close);
+}
+
+
+
+SimpleApp::SimpleApp() {}
+
+void SimpleApp::OnContextInitialized() {
+	CEF_REQUIRE_UI_THREAD();
+
+	CefRefPtr<CefCommandLine> command_line =
+		CefCommandLine::GetGlobalCommandLine();
+
+#if defined(OS_WIN) || defined(OS_LINUX)
+	// Create the browser using the Views framework if "--use-views" is specified
+	// via the command-line. Otherwise, create the browser using the native
+	// platform framework. The Views framework is currently only supported on
+	// Windows and Linux.
+	const bool use_views = command_line->HasSwitch("use-views");
+#else
+	const bool use_views = false;
+#endif
+
+	// SimpleHandler implements browser-level callbacks.
+	CefRefPtr<SimpleHandler> handler(new SimpleHandler(use_views));
+
+	// Specify CEF browser settings here.
+	CefBrowserSettings browser_settings;
+
+	std::string url;
+
+	// Check if a "--url=" value was provided via the command-line. If so, use
+	// that instead of the default URL.
+	url = command_line->GetSwitchValue("url");
+	if (url.empty())
+		url = "http://www.google.com";
+
+	if (use_views) {
+		// Create the BrowserView.
+		CefRefPtr<CefBrowserView> browser_view = CefBrowserView::CreateBrowserView(
+			handler, url, browser_settings, NULL, NULL);
+
+		// Create the Window. It will show itself after creation.
+		CefWindow::CreateTopLevelWindow(new SimpleWindowDelegate(browser_view));
+	}
+	else {
+		// Information used when creating the native window.
+		CefWindowInfo window_info;
+
+#if defined(OS_WIN)
+		// On Windows we need to specify certain flags that will be passed to
+		// CreateWindowEx().
+		window_info.SetAsPopup(NULL, "cefsimple");
+#endif
+
+		// Create the first browser window.
+		CefBrowserHost::CreateBrowser(window_info, handler, url, browser_settings,
+			NULL);
+	}
+}
+
+
 #define MAX_LOADSTRING 100
 
 HINSTANCE hInst;                                // current instance
